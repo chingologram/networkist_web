052cd6066811ecc703d61e56bdf8a724754a41ae
Nicolas Mercado <nicolas.fernando.mercado@gmail.com>
2024-08-06 19:43:16 -0300

    Fix basepath

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Configure `pageExtensions` to include MDX files
+    // Solo para Github Pages
+    basePath: '/networkist_web',
     output: 'export',
     defaultLayout: true,
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
